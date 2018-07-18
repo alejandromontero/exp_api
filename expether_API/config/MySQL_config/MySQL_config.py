e23d32fbfd86039b56fecfac347e4189cedc2cbc
alejandromontero <alejandro.montero.rivero@gmail.com>
2018-07-18 11:20:05 +0200

    ADD EEM wrapper

--- expether_API/config/MySQL_config/MySQL_config.py
+++ expether_API/config/MySQL_config/MySQL_config.py
@@ -1,7 +1,8 @@
 workload_mapping = {
         "id": "int",
         "name": "varchar(255)",
-        "requirements": "varchar(255)",
+        "requirement": "varchar(255)",
+        "assigned_to": "varchar(255)",
         "active": "tinyint(1)"
         }
 
@@ -9,5 +10,19 @@ box_mapping = {
         "id": "varchar(255)",
         "hardware": "varchar(255)",
         "model": "varchar(255)",
-        "quantity": "int"
+        "pcie_vendor_id": "varchar(255)",
+        "pcie_device_id": "varchar(255)",
+        "assigned_to":  "varchar(255)"
+        }
+
+net_card_mapping = {
+        "id": "varchar(255)",
+        "gid": "int",
+        "assigned_to": "varchar(255)"
+        }
+
+servers_mapping = {
+        "id": "varchar(255)",
+        "name": "varchar(255)",
+        "number": "int"
         }
--- expether_API/services/eem/eem.py
+++ expether_API/services/eem/eem.py
@@ -1,14 +1,21 @@
 import os
-
-__eemcly = os.path.join(
-        os.path.dirname(__file__),
-        '..', 'eemcli', 'eemcli.py'
-        )
+from subprocess import (
+        Popen,
+        PIPE
+    )
 
 
 class EEMFactory(object):
-    def __init__(self, conf):
+    def __init__(self, conf, clientPath):
         self.conf = conf
+        self.clientBasePath = clientPath
+        self.clientPath = os.path.join(
+                self.clientBasePath,
+                "eemcli", "eemcli.py"
+                )
+
+    def get_base_cmd(self):
+        return ['python2', self.clientPath, '-c', self.conf]
 
 
 class EEM(object):
@@ -16,5 +23,23 @@ class EEM(object):
             self,
             EEM_factory: EEMFactory,
     ):
-        self.EEM_factory = EEMFactory
-        self.instance = None
+        self.EEM_factory = EEM_factory
+        self.cmd = None
+
+    def make_request(self, expanded_cmd):
+        return Popen(
+                self.get_cmd()+expanded_cmd,
+                stdout=PIPE).communicate()[0].decode('ascii')
+
+    def get_cmd(self):
+        if not self.cmd:
+            self.cmd = self.EEM_factory.get_base_cmd()
+        return self.cmd
+
+    def get_list(self):
+        expanded_cmd = ['get', '--list']
+        return self.make_request(expanded_cmd)
+
+    def get_box_info(self, id):
+        expanded_cmd = ['get', '--id', id]
+        return self.make_request(expanded_cmd)
