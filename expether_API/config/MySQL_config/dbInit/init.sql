36d7df481a57b8abacfa8731e00c0996b6e5c362
alejandromontero <alejandro.montero.rivero@gmail.com>
2018-09-12 17:44:51 +0200

    Add new capacity tables to DB

--- expether_API/config/MySQL_config/dbInit/init.sql
+++ expether_API/config/MySQL_config/dbInit/init.sql
@@ -18,12 +18,20 @@ CREATE TABLE IF NOT EXISTS workloads (
 	FOREIGN KEY (assigned_to) REFERENCES servers(name)
 	);
 
-CREATE TABLE IF NOT EXISTS requirements (
+CREATE TABLE IF NOT EXISTS hardware_requirements (
 	workload_id int NOT NULL,
 	hardware_type varchar(255) NOT NULL,
 	model varchar(255),
-	capacity int,
 	PRIMARY KEY (workload_id,hardware_type),
+	FOREIGN KEY (workload_id) REFERENCES workloads(id)
+	);
+
+CREATE TABLE IF NOT EXISTS capacity_requirements (
+	workload_id int NOT NULL,
+	requirement_name varchar(255) NOT NULL,
+	unit varchar(255),
+	value int NOT NULL,
+	PRIMARY KEY(workload_id,requirement_name),
 	FOREIGN KEY (workload_id) REFERENCES workloads(id)
 	);
 
@@ -45,11 +53,33 @@ CREATE TABLE IF NOT EXISTS hardware_cards (
 	PRIMARY KEY (id)
 	);
 
+CREATE TABLE IF NOT EXISTS hardware_capacity (
+	hardware_id varchar(255) NOT NULL,
+	capacity_name varchar(255) NOT NULL,
+	unit varchar(255),
+	value int NOT NULL,
+	PRIMARY KEY(hardware_id,capacity_name),
+	FOREIGN KEY(hardware_id) REFERENCES hardware_cards(id)
+	);
+
 CREATE TABLE IF NOT EXISTS assignments (
 	hardware_card varchar(255) NOT NULL,
 	server_card varchar (255) NOT NULL,
 	workload int NOT NULL,
 	PRIMARY KEY (hardware_card,server_card,workload),
+	FOREIGN KEY (hardware_card) REFERENCES hardware_cards(id),
+	FOREIGN KEY (server_card) REFERENCES net_card(id),
+	FOREIGN KEY (workload) REFERENCES workloads(id)
+	);
+
+CREATE TABLE IF NOT EXISTS assigned_capacity (
+	hardware_card varchar(255) NOT NULL,
+	server_card varchar (255) NOT NULL,
+	workload int NOT NULL,
+	capacity_name varchar(255) NOT NULL,
+	unit varchar(255) NOT NULL,
+	value int,
+	PRIMARY KEY (hardware_card,server_card,workload,capacity_name),
 	FOREIGN KEY (hardware_card) REFERENCES hardware_cards(id),
 	FOREIGN KEY (server_card) REFERENCES net_card(id),
 	FOREIGN KEY (workload) REFERENCES workloads(id)
