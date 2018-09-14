8a3e08131569393c75a6219c55765503c835b7ae
alejandromontero <alejandro.montero.rivero@gmail.com>
2018-09-14 15:55:14 +0200

    Add DB transactions for DB modifications

--- expether_API/services/mysql/mysqlDB.py
+++ expether_API/services/mysql/mysqlDB.py
@@ -27,30 +27,77 @@ class MySQL(object):
             MySQL_factory: MySQLFactory,
     ):
         self.MySQL_factory = MySQL_factory
-        self.instance = None
+        self.db_instance = None
+        self.transaction_mode = False
+        self.transaction = []
 
     def connection(self) -> MySQLConnection:
-        if not self.instance:
-            self.instance = self.MySQL_factory.create()
+        if not self.db_instance:
+            self.db_instance = self.MySQL_factory.create()
+            self.db_instance.autocommit = False
 
-        return self.instance
+    def close_connection(self, cursor):
+        cursor.close()
+
+    def get_cursor(self):
+        return self.db_instance.cursor()
+
+    def start_transaction(self):
+        self.transaction_mode = True
+
+    def commit_transaction(self):
+        self.transaction_mode = False
+        cursor = self.get_cursor()
+        result = []
+        try:
+            for query in self.transaction:
+                if "values" in query:
+                    cursor.execute(query["statement"], query["values"])
+                else:
+                    cursor.execute(query["statement"])
+                values = []
+                for entry in cursor:
+                    values.append(entry)
+                result.append(values)
+            self.db_instance.commit()
+        except (Error, Warning) as err:
+            self.close_connection(cursor)
+            return self.rollback("DB error: {}".format(err))
+
+        self.close_connection(cursor)
+        # For simple SELECT queries, return the result directly
+        if len(self.transaction) == 1:
+            self.transaction = []
+            return (True, result[0])
+        else:
+            self.transaction = []
+            return (True, result)
+
+    # Used only to commit SELECT transactions that do not modify the DB
+    def commit_nontransaction_select(self, query):
+        cursor = self.get_cursor()
+        result = []
+        try:
+            cursor.execute(query)
+            for entry in cursor:
+                result.append(entry)
+            self.db_instance.commit()
+            self.close_connection(cursor)
+        except (Error, Warning) as err:
+            self.close_connection(cursor)
+            return None
+
+        return result
+
+    def rollback(self, error):
+        self.transaction = []
+        self.db_instance.rollback()
+        return (False, error)
 
     def select_query(self, statement):
-        cnx = self.connection()
-        cursor = cnx.cursor()
-        try:
-            cursor.execute(statement)
-        except (Error, Warning) as err:
-            print (err)
-
-        values = []
-        for entry in cursor:
-            values.append(entry)
-        cursor.close()
-        cnx.commit()
-        cursor.close()
-
-        return values
+        self.connection()
+        result = self.commit_nontransaction_select(statement)
+        return result
 
     def insert_query(self, table, mapping, data):
         if len(mapping) != len(data):
@@ -78,16 +125,12 @@ class MySQL(object):
             values[mapping[-1]] = '%s' % (data[-1])
         else:
             values[mapping[-1]] = None
-        cnx = self.connection()
-        cursor = cnx.cursor()
-        try:
-            cursor.execute(statement, values)
-        except (Error, Warning) as err:
-            return (False, str(err))
 
-        cursor.close()
-        cnx.commit()
-        cursor.close()
+        self.connection()
+        self.transaction.append({"statement": statement, "values": values})
+        if not self.transaction_mode:
+            result = self.commit_transaction()
+
         return (True, "OK")
 
     def modify_query(self, table, mapping, data, modID, modValue):
@@ -106,17 +149,11 @@ class MySQL(object):
         statement += '%s = "%s" ' % (mapping[-1], data[-1])
         statement += 'WHERE %s = "%s"' % (modID, modValue)
 
-        cnx = self.connection()
-        cursor = cnx.cursor()
-
-        try:
-            cursor.execute(statement)
-        except (Error, Warning) as err:
-            return (False, str(err))
-
-        cursor.close()
-        cnx.commit()
-        cursor.close()
+        self.connection()
+        self.transaction.append({"statement": statement})
+        if not self.transaction_mode:
+            result = self.commit_transaction()
+            self.close_connection()
 
         return (True, "OK")
 
@@ -139,17 +176,10 @@ class MySQL(object):
             statement += '%s = "%s" AND ' % (modIDs[x], modValues[x])
         statement += '%s = "%s"' % (modIDs[-1], modValues[-1])
 
-        cnx = self.connection()
-        cursor = cnx.cursor()
-
-        try:
-            cursor.execute(statement)
-        except (Error, Warning) as err:
-            return (False, str(err))
-
-        cursor.close()
-        cnx.commit()
-        cursor.close()
+        self.connection()
+        self.transaction.append({"statement": statement})
+        if not self.transaction_mode:
+            result = self.commit_transaction()
 
         return (True, "OK")
 
@@ -165,17 +195,11 @@ class MySQL(object):
             statement += '%s = "%s" AND ' % (mapping[x], data[x])
         statement += '%s = "%s"' % (mapping[-1], data[-1])
 
-        cnx = self.connection()
-        cursor = cnx.cursor()
+        self.connection()
+        self.transaction.append({"statement": statement})
+        if not self.transaction_mode:
+            result = self.commit_transaction()
 
-        try:
-            cursor.execute(statement)
-        except (Error, Warning) as err:
-            return (False, str(err))
-
-        cursor.close()
-        cnx.commit()
-        cursor.close()
         return (True, "OK")
 
     def delete_query_simple(self, table, field, value):
@@ -184,15 +208,10 @@ class MySQL(object):
             'WHERE %s = "%s"'
             ) % (table, field, value)
 
-        cnx = self.connection()
-        cursor = cnx.cursor()
 
-        try:
-            cursor.execute(statement)
-        except (Error, Warning) as err:
-            return (False, str(err))
+        self.connection()
+        self.transaction.append({"statement": statement})
+        if not self.transaction_mode:
+            result = self.commit_transaction()
 
-        cursor.close()
-        cnx.commit()
-        cursor.close()
         return (True, "OK")
